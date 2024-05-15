471ccde35118d751191cc5d087993c6c7b8defe1
Kalebbug <167897785+Kalebbug@users.noreply.github.com>
2024-05-15 13:51:53 -0400

    Add files via upload

---
+++ Concacaf Final project/script.js
@@ -0,0 +1,13 @@
+
+ 
+
+
+  document.getElementById("prediction-form").addEventListener("submit", function(event) {
+    event.preventDefault(); 
+  
+
+    var matchPrediction = document.getElementById("match").value;
+    
+    alert("Prediction submitted successfully!");
+  });
+  
